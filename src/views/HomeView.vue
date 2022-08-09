3f0add4eff3f032af30aef76132c5367895b4c2e
Naruê G. Santos <narue.goncalves@warren.com.br>
2022-08-09 14:48:13 -0300

    feat: add view components.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HomeHeader />
+    <PostBox @edit="editPost" :sinal="sinal" />
   </div>
 </template>
-
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import RegisterModal from "@/components/Home/RegisterModal.vue";
+import EditModal from "@/components/Home/EditModal.vue";
+import HomeHeader from "@/components/Home/HomeHeader.vue";
+import PostBox from "@/components/Home/PostBox.vue";
 @Component({
   components: {
-    HelloWorld,
+    HomeHeader,
+    PostBox,
+    RegisterModal,
+    EditModal,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  currentPost = {};
+  edit = false;
+  sinal = false;
+  editPost(value: object): void {
+    this.currentPost = value;
+    this.edit = true;
+  }
+}
 </script>
+
+<style lang="scss">
+.home {
+  z-index: 1;
+}
+.leave {
+  animation-name: leave;
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+}
+@keyframes leave {
+  from {
+    margin-left: 0%;
+  }
+  to {
+    margin-left: 50%;
+  }
+}
+</style>
