9571ca853d84df6b70f20e34569b4801fdf0fdff
Naruê G. Santos <narue.goncalves@warren.com.br>
2022-08-09 14:46:53 -0300

    feat: add font awesome icons.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faCommentDots,
+  faMessage,
+  faXmark,
+  faEllipsis,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCommentDots, faMessage, faXmark, faEllipsis);
+Vue.component("fa-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
