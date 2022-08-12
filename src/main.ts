5f1dd247c1dce063074396510b703868faa8fda9
Naruê G. Santos <narue.goncalves@warren.com.br>
2022-08-11 21:43:55 -0300

    feat: add maska

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import Maska from "maska";
+Vue.use(Maska);
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
